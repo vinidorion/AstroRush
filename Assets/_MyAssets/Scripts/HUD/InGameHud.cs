9b182f12d9aefe30271f1dc3fae493776abbb379
ali bakir <143757270+bigdanglingballs@users.noreply.github.com>
2024-02-15 11:32:22 -0500

    InGameHud
    script ajouté

---
+++ Assets/_MyAssets/Scripts/HUD/InGameHud.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameHud : MonoBehaviour
+{
+	void Update()
+	{
+		
+	}
+
+
+	// les fonctions dessous s'occupent de ceci: https://github.com/vinidorion/AstroRush/blob/main/Autre/ingame_hud.PNG
+
+	// draw la vie du joueur
+	private void HP()
+	{
+
+	}
+
+	// draw la position (premier, deuxième, etc)
+	private void Pos()
+	{
+
+	}
+
+	// draw l'icone de l'item du joueur
+	private void Item()
+	{
+
+	}
+
+	// draw la map en 2D (vue de haut)
+	private void Map()
+	{
+
+	}
+
+	// draw la progression sur la track (utiliser le current checkpoint du joueur sur le nb total de checkpoint)
+	private void Prog()
+	{
+
+	}
+
+	// draw time comparison, your current lap time vs your best lap time
+	// s'affiche pendant quelques secondes
+	// rouge si plus lent (ex: +00:01:00 si une seconde plus long que le meilleur score)
+	// vert si plus rapide (ex: -00:01:00 si une seconde plus court que le meilleur score)
+	private void TimeComp()
+	{
+
+	}
+
+}
