b7c266a817cccc96c6ee6ea0b439e9815a655181
vinidorion <103599630+vinidorion@users.noreply.github.com>
2024-02-15 11:40:40 -0500

    ...

--- Assets/_MyAssets/Scripts/Player.cs
+++ Assets/_MyAssets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float inputx = 0;
+    float inputz = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move();
     }
+
+    private void move()
+    {
+        inputx = Input.GetAxis("Horizontal");
+        inputz = Input.GetAxis("Vertical");
+        if (inputz > 0) SpaceShip.Instance.Forward();
+        if (inputx > 0) SpaceShip.Instance.Turn(false);
+        if (inputx < 0) SpaceShip.Instance.Turn(true);
+        if (Input.GetKey(KeyCode.Mouse0)) SpaceShip.Instance.AirBrake(true);
+        if (Input.GetKey(KeyCode.Mouse1)) SpaceShip.Instance.AirBrake(false);
+    }    
 }
--- Assets/_MyAssets/Scripts/SpaceShip.cs
+++ Assets/_MyAssets/Scripts/SpaceShip.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class SpaceShip : MonoBehaviour
 {
+    public static SpaceShip Instance;
+
     bool freeze = false;
-    float current_speed = 0;
+    Vector3 current_speed = Vector3.zero;
     int current_pu = 0;
 
     [Header("Stats")]
@@ -18,6 +20,7 @@ public class SpaceShip : MonoBehaviour
 
     [SerializeField] Rigidbody rb = default;
 
+<<<<<<< Updated upstream
     // variables pour détection du sol
 	[SerializeField] private LayerMask _layersToHit;    // dans l'inspecteur mettre la layer "track"
 	private float _maxDist = 5f;                        // distance maximale du raycast
@@ -43,49 +46,75 @@ public class SpaceShip : MonoBehaviour
 	}
 
     void Forward()
+=======
+    private void Awake()
+>>>>>>> Stashed changes
     {
-        if (!freeze && current_speed < max_speed)
+        Instance = this; //associe l'instance de Player au script
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        AirResistance();
+        current_speed = rb.velocity;
+        Debug.Log(current_speed);
+    }
+
+    private void AirResistance()
+    {
+        Vector3 force = new Vector3(-max_speed * (current_speed.x / max_speed), -max_speed * (current_speed.y / max_speed), -max_speed * (current_speed.z / max_speed))/2;
+        rb.AddForce(force);
+    }
+
+    public void Forward()
+    {
+        if (!freeze && current_speed.y < max_speed)
         {
             Vector3 force = new Vector3(-1 * acceleration, 0, 0);
             rb.AddForce(force);
         }
     }
 
-    void Turn(bool left)
+    public void Turn(bool left)
     {
         if (!freeze)
         {
             if (left)
             {
-                float rotation = agility - current_speed;
+                float rotation = agility - current_speed.y;
             }
             if (!left)
             {
-                float rotation = (agility - current_speed) * -1;
+                float rotation = (agility - current_speed.y) * -1;
             }
         }
     }
 
-    void AirBrake(bool left)
+    public void AirBrake(bool left)
     {
         if (!freeze)
         {
             if (left)
             {
-                float rotation = agility - current_speed + airbrake_power;
-                Vector3 force = new Vector3(1 * current_speed + airbrake_power - weigth, 0, 0);
+                float rotation = agility - current_speed.y + airbrake_power;
+                Vector3 force = new Vector3(1 * current_speed.y + airbrake_power - weigth, 0, 0);
                 rb.AddForce(force);
             }
             if (!left)
             {
-                float rotation = (agility - current_speed + airbrake_power) * -1;
-                Vector3 force = new Vector3(1 * current_speed + airbrake_power - weigth, 0, 0);
+                float rotation = (agility - current_speed.y + airbrake_power) * -1;
+                Vector3 force = new Vector3(1 * current_speed.y + airbrake_power - weigth, 0, 0);
                 rb.AddForce(force);
             }
         }
     }
 
-    void UsePU()
+    public void UsePU()
     {
         if (!freeze)
         {
